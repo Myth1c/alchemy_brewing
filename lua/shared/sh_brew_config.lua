1a2142fabebefa6c83430cccb5f6eba2fb0d306b
Myth1c <Alemsm1@gmail.com>
2022-01-27 18:24:36 -0500

    Updated config defaults

--- lua/shared/sh_brew_config.lua
+++ lua/shared/sh_brew_config.lua
@@ -10,15 +10,15 @@ Brew_Config = {
     ---  UI SECTION  ---
     --------------------
     -- Font for all text displayed on the display. Default "HudSelectionText"
-    GUI_Font = "HudSelectionText",
+    GUI_Font = "DermaLarge",
     -- Color for the text. Default Color(255, 255, 255, 255)
     GUI_Font_mainColour = Color(255, 255, 255, 255),
     -- Color for the shadow/backdrop of the text. Default Color(119, 135, 137, 255)
     GUI_Font_shadowColour = Color(119, 135, 137, 255),
     -- Color for the foreground of the main frame. Default Color(120, 120, 120, 0)
-    GUI_Frame_Foreground = Color(120, 120, 120, 0),
+    GUI_Frame_Foreground = Color(120, 120, 120, 255),
     -- Color for the background of the main frame. Default Color(0, 0, 0, 0)
-    GUI_Frame_Background = Color(0, 0, 0, 0),
+    GUI_Frame_Background = Color(0, 0, 0, 255),
     -- Color for the background of the progress bar. Default Color(60, 60, 60, 180)
     GUI_ProgressBar_Background = Color(60, 60, 60, 180),
     -- Color for the border on the progress bar. Default Color(0, 0, 0, 180)
