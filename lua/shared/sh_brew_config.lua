bb366219e673870a9d093fcd5dfa8cbecd3f927e
Myth1c <Alemsm1@gmail.com>
2022-01-26 20:15:19 -0500

    Added Derma settings to config

--- lua/shared/sh_brew_config.lua
+++ lua/shared/sh_brew_config.lua
@@ -6,7 +6,27 @@ CONFIG = {
     Item_Max = 10,
     Unique_Max = 15,
     
-
+    --------------------
+    ---  UI SECTION  ---
+    --------------------
+    -- Font for all text displayed on the display. Default "HudSelectionText"
+    GUI_Font = "HudSelectionText",
+    -- Color for the text. Default Color(255, 255, 255, 255)
+    GUI_Font_mainColour = Color(255, 255, 255, 255),
+    -- Color for the shadow/backdrop of the text. Default Color(119, 135, 137, 255)
+    GUI_Font_shadowColour = Color(119, 135, 137, 255),
+    -- Color for the foreground of the main frame. Default Color(120, 120, 120, 0)
+    GUI_Frame_Foreground = Color(120, 120, 120, 0),
+    -- Color for the background of the main frame. Default Color(0, 0, 0, 0)
+    GUI_Frame_Background = Color(0, 0, 0, 0),
+    -- Color for the background of the progress bar. Default Color(60, 60, 60, 180)
+    GUI_ProgressBar_Background = Color(60, 60, 60, 180),
+    -- Color for the border on the progress bar. Default Color(0, 0, 0, 180)
+    GUI_ProgressBar_Border = Color(0,0,0, 180),
+    -- Color for the moving part of the progress bar. Default Color(255, 255, 255, 180)
+    GUI_ProgressBar_BarColor = Color(255, 255, 255, 180),
+    -- How much the UI should be smoothed out for curves. Default 10
+    GUI_FrameCurve = 10,
 
 
 }
