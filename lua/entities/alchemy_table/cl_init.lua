9fc333e0bacfec71fa8619b116a4b2a383b0d158
Myth1c <Alemsm1@gmail.com>
2022-01-27 18:24:22 -0500

    Updated client file to close UI with more conds

--- lua/entities/alchemy_table/cl_init.lua
+++ lua/entities/alchemy_table/cl_init.lua
@@ -4,6 +4,15 @@ include("sh_init.lua")
 
 function ENT:Draw()
 	self:DrawModel()
+
+
+	if IsValid(brewFrame) then
+
+		if math.DistanceSqr(LocalPlayer():GetPos().x, LocalPlayer():GetPos().y, self:GetPos().x, self:GetPos().y) > 9000 then 
+			brewFrame:Close()
+		end
+	end
+
 end
 
 
@@ -11,6 +20,7 @@ net.Receive("open_brewUI", function(ply, len)
 
 	if net.ReadString() ~= "brewUI" then return end
 
-	if not IsValid(brewFrame) then DrawBrewing() end
+	if not IsValid(brewFrame) then DrawBrewing(ent)
+	elseif IsValid(brewFrame) then brewFrame:Close() end
 
 end)
