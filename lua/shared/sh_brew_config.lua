cdb496828a932e87fb8e9b295cc3eb31163de332
Myth1c <Alemsm1@gmail.com>
2022-01-28 03:19:34 -0500

    Added more settings to config

--- lua/shared/sh_brew_config.lua
+++ lua/shared/sh_brew_config.lua
@@ -7,18 +7,24 @@ Brew_Config = {
     Unique_Max = 15,
     
     GUI_Font = "DermaLarge",
-    
     GUI_Font_mainColour = Color(255, 255, 255, 255),
-   
     GUI_Font_shadowColour = Color(119, 135, 137, 255),
-    
-    GUI_Frame_Foreground = Color(120, 120, 120, 255),
-   
-    GUI_Frame_Background = Color(0, 0, 0, 255),
-    
-    GUI_BrewSlot_Background = Color(60, 60, 60, 180),
-        
     GUI_FrameCurve = 10,
+
+    GUI_Brew_Foreground = Color(120, 120, 120, 255),
+    GUI_Brew_Background = Color(0, 0, 0, 255),
+    GUI_BrewSlot_Background = Color(60, 60, 60, 255),
+    GUI_BrewSlot_Image = "decals/light",
+
+    Inventory_Size = 25,
+    Inventory_BoxSize = 50,
+    Inventory_Padding = 5,
+
+
+    GUI_Inventory_Foreground = Color(50, 50, 50, 255),
+    GUI_Inventory_Background = Color(0, 0, 0, 255),
+    GUI_Inventory_Header = Color(150, 150, 150, 255)
+
 
 
 }
