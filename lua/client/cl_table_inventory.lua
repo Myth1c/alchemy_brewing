e038cfdf8e3702b9dc5fb13397e9acee6ed74755
Myth1c <Alemsm1@gmail.com>
2022-01-28 03:20:29 -0500

    Updated to use config, removed unecessary vars

--- lua/client/cl_table_inventory.lua
+++ lua/client/cl_table_inventory.lua
@@ -11,18 +11,21 @@ local stored_ents = {
 local FontType = Brew_Config.GUI_Font or "DermaLarge"
 local FontColour = Brew_Config.GUI_Font_mainColour or Color(255, 255, 255, 255)
 local FontColourShadow = Brew_Config.GUI_Font_shadowColour or Color(119, 135, 137, 255)
-local FramePrimaryColour = Brew_Config.GUI_Frame_Foreground or Color(120,120,120, 0)
-local FrameBorderColour = Brew_Config.GUI_Frame_Background or Color(0,0,0, 0)
 local FrameCurve = Brew_Config.FrameCurve or 10
 
-local BrewSlotBackground = Brew_Config.GUI_BrewSlot_Background or Color(60,60,60, 180)
+local SlotBackground = Brew_Config.GUI_BrewSlot_Background or Color(60,60,60, 180)
+
+local PrimaryColour = Brew_Config.GUI_Inventory_Foreground or Color(120,120,120, 0)
+local BorderColour = Brew_Config.GUI_Inventory_Background or Color(0,0,0, 0)
+local HeaderColor = Brew_Config.GUI_Inventory_Header or Color(255, 255, 255, 255)
+
 
 
 function DrawStorage()
 
-    local max = 25
-    local box = 50
-    local padding = 5
+    local max = Brew_Config.Inventory_Size or 25
+    local box = Brew_Config.Inventory_BoxSize or 50
+    local padding = Brew_Config.Inventory_Padding or 5
     
     local factor = GetBiggestFactor(max)
 
@@ -44,8 +47,15 @@ function DrawStorage()
     storageFrame:MakePopup()
     storageFrame:SetKeyboardInputEnabled(false)
     storageFrame.Paint = function(s, w, h)
-        draw.RoundedBox(FrameCurve, 0, 0, w, h, FrameBorderColour)
-        draw.RoundedBox(FrameCurve, 2, 2, w-4, h-4, FramePrimaryColour)
+
+
+        draw.RoundedBox(FrameCurve, 0, 0, w, h, BorderColour)
+        draw.RoundedBox(FrameCurve, 2, 2, w-4, h-4, PrimaryColour)
+
+        draw.RoundedBox(FrameCurve, 0, 0, w, 25, BorderColour)
+        draw.RoundedBox(FrameCurve, 2, 2, w-4, 21, HeaderColor)
+
+        draw.RoundedBox(FrameCurve, 2, 27, w-4, h-29, BorderColour)
     end
 
 
@@ -62,6 +72,21 @@ function CreateInventorySlot(sizeX, sizeY, posX, posY, max, current, padding, co
 
     local pos = Vector(GetNext(padding, current, max, posX, posY, sizeX, sizeY, columns))
 
+    local slotPad = vgui.Create("DFrame", storageFrame)
+    slotPad:SetVisible(true)
+    slotPad:SetTitle("")
+    slotPad:ShowCloseButton(false)
+    slotPad:SetDraggable(false)
+    slotPad:SetSize(sizeX + 6, sizeY + 6)
+    slotPad:SetPos(pos.x - 3, (pos.y - 3) + 25)
+
+    slotPad.Paint = function(s, w, h)
+
+        draw.RoundedBox(FrameCurve, 0, 0, w, h, BorderColour)
+        draw.RoundedBox(FrameCurve, 2, 2, w-4, h-4, PrimaryColour)
+
+    end
+
     local invSlot = vgui.Create("DImageButton", storageFrame)
     invSlot:SetPos(pos.x, pos.y + 25)
     invSlot:SetSize(sizeX, sizeY)
@@ -69,8 +94,8 @@ function CreateInventorySlot(sizeX, sizeY, posX, posY, max, current, padding, co
     invSlot:SetImage("decals/light")
 
     invSlot.Paint = function(s, w, h)
-        draw.RoundedBox(FrameCurve, 0, 0, w, h, FrameBorderColour)
-        draw.RoundedBox(FrameCurve, 2, 2, w-4, h-4, BrewSlotBackground)
+        draw.RoundedBox(FrameCurve, 0, 0, w, h, BorderColour)
+        draw.RoundedBox(FrameCurve, 2, 2, w-4, h-4, SlotBackground)
     end
 
     return invSlot
