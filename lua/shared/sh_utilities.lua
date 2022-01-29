93e8b2498a8e5e0677ca9c5f76af3c54320030e4
Myth1c <Alemsm1@gmail.com>
2022-01-29 16:39:10 -0500

    Added Custom Font for Status Effects

--- lua/shared/sh_utilities.lua
+++ lua/shared/sh_utilities.lua
@@ -5,4 +5,26 @@ if SERVER then
     util.AddNetworkString("brew_draw_StatusUI")
 
     util.AddNetworkString("brew_drop_item")
+
+
+else
+
+surface.CreateFont( "Brew_StatusFont", {
+	font = "Impact", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
+	extended = false,
+	size = 26,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+} )
+
 end
