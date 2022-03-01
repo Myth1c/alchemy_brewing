141c51060c2c45d042fc12848747ea122436019c
Myth1c <Alemsm1@gmail.com>
2022-03-01 16:55:20 -0500

    Updated speed effect to scale based off tier.

--- lua/shared/sh_effect_functions.lua
+++ lua/shared/sh_effect_functions.lua
@@ -8,13 +8,16 @@ if SERVER then
     
     function Effects_Speed(ply, pot, tier, time)
 
-        DebugPrint("Applying speed to: " .. tostring(ply) .. "\nTier: " .. tier .. "\nTime Limit: " .. time)
+        time = time * tier
+
+        local boost = 1 + (0.25 * tier)
+
+        DebugPrint("Applying speed to: " .. tostring(ply) .. "\nTier: " .. tier .. "\nTime Limit: " .. time .. "\nSpeed Multiplier: " .. boost)
         
 	    pot:PotionNetworkMessage(ply, "speed", tier, time)
 
-        ply:SetRunSpeed(ply:GetRunSpeed() * 1.5)
+        ply:SetRunSpeed(ply:GetRunSpeed() * boost)
 
-        --timer.Simple(time, function() ply:SetRunSpeed(ply:GetRunSpeed() / 1.5) end)
 
     end
 
@@ -28,12 +31,10 @@ if SERVER then
 
     net.Receive("brew_clear_single_effect", function(len, ply)
     
-        print("Net message received to clear an effect.")
-
         local effect = net.ReadString()
 
         if effect == "speed" then
-            ply:SetRunSpeed(ply:GetRunSpeed() / 1.5)
+            ply:SetRunSpeed(400)
         end
     
     end)
