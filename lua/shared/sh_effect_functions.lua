10d3621321619c40295c63966acc8ffefe403c80
Myth1c <Alemsm1@gmail.com>
2022-03-01 16:20:38 -0500

    Updated new speed effect function to apply speed and remove it after the time limit.

--- lua/shared/sh_effect_functions.lua
+++ lua/shared/sh_effect_functions.lua
@@ -6,11 +6,34 @@ if SERVER then
 
     end
     
-    function Effects_Speed(ply, pot)
+    function Effects_Speed(ply, pot, tier, time)
 
-        DebugPrint("Applying speed to: " .. ply)
+        DebugPrint("Applying speed to: " .. tostring(ply) .. "\nTier: " .. tier .. "\nTime Limit: " .. time)
         
-	    pot:PotionNetworkMessage(ply, "speed", 1, 10)
+	    pot:PotionNetworkMessage(ply, "speed", tier, time)
+
+        ply:SetRunSpeed(ply:GetRunSpeed() * 1.5)
+
+        Timer.Simple(time, function() ply:SetRunSpeed(ply:GetRunSpeed() * 1.5) end)
+
+    end
+
+    net.Receive("brew_effect_cleared", function(len, ply)
+    
+        DebugPrint("Removing all effects from:" .. ply)
+
+        ClearEffects(ply)
+    
+    end)
+
+
+
+
+
+
+    function ClearEffects(player)
+
+        ply:SetRunSpeed(ply:GetRunSpeed() / 1.5)
 
     end
 
