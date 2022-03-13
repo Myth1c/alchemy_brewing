3939a4f198aba89bc8ca3b3b6ab775da10daf96c
Myth1c <Alemsm1@gmail.com>
2022-03-13 04:11:07 -0400

    Expanded help menu to include tips

--- lua/client/cl_help_menu.lua
+++ lua/client/cl_help_menu.lua
@@ -1,6 +1,4 @@
 helpFrame = {}
-local openAnimation = nil
-local closeAnimation = nil
 --[[
     This chunk of code initializes settings and in case the config file doesn't load, it will set to defaults seen after the "or" statements
 ]]--
@@ -22,7 +20,7 @@ function DrawHelpMenu()
 
     helpFrame = vgui.Create("DFrame")
     helpFrame:SetPos(ScrW() * 680/1920, ScrH() * 95/1080)
-    helpFrame:SetSize( ScrW() * 560/1920, ScrH() * 110/1080 )
+    helpFrame:SetSize( ScrW() * 560/1920, ScrH() * 210/1080 )
     helpFrame:SetVisible(true)
     helpFrame:SetTitle("")
     helpFrame:ShowCloseButton(false)
@@ -33,83 +31,99 @@ function DrawHelpMenu()
         draw.RoundedBox(FrameCurve, 2, 2, w-4, h-4, FramePrimaryColour)
     end
 
-    local htuTitle = vgui.Create("DLabel", helpFrame)
-    htuTitle:SetFont(TitleFont)
-    htuTitle:SetText("How to use:")
-    htuTitle:SetSize( ScrW() * 300/1920, ScrH() * 40/1080 )
-    htuTitle:SetPos(ScrW() * (210 - string.len(htuTitle:GetText()))/1920, ScrH() * 0/1080)
-    htuTitle:SetTextColor(FontColour)
-    htuTitle:AlignTop(0)
-    
+    local textBox = vgui.Create("DLabel", helpFrame)
+    textBox:SetFont(BodyFont)
+    textBox:SetText("")
+    textBox:SetSize( ScrW() * 680/1920, ScrH() * 210/1080 )
+    textBox:SetPos(ScrW() * 0/1920, ScrH() * 0/1080)
+    textBox:SetTextColor(FontColour)
+    textBox.Paint = function(s, w, h)
+        
+        HowToTitle = {}
+        HowToTitle["pos"] = {265, 0}
+        HowToTitle["color"] = FontColour
+        HowToTitle["text"] = "How to use:"
+        HowToTitle["font"] = TitleFont
+        HowToTitle["xalign"] = TEXT_ALIGN_CENTER
+        HowToTitle["yalign"] = TEXT_ALIGN_TOP
 
-    local htuLine1 = vgui.Create("DLabel", helpFrame)
-    htuLine1:SetFont(BodyFont)
-    htuLine1:SetText("")
-    htuLine1:SetSize( ScrW() * 600/1920, ScrH() * 125/1080 )
-    htuLine1:SetPos(ScrW() * 0/1920, ScrH() * 0/1080)
-    htuLine1:SetTextColor(FontColour)
-    htuLine1.Paint = function(s, w, h)
-        
-        struc = {}
-        struc["pos"] = {265, 40}
-        struc["color"] = FontColour
-        struc["text"] = "Gather ingredients from around the world. Once you have some, return to"
-        struc["font"] = BodyFont
-        struc["xalign"] = TEXT_ALIGN_CENTER
-        struc["yalign"] = TEXT_ALIGN_TOP
+        HowTo1 = {}
+        HowTo1["pos"] = {265, 40}
+        HowTo1["color"] = FontColour
+        HowTo1["text"] = "Gather ingredients from around the world. Once you have some, return to"
+        HowTo1["font"] = BodyFont
+        HowTo1["xalign"] = TEXT_ALIGN_CENTER
+        HowTo1["yalign"] = TEXT_ALIGN_TOP
+
+        HowTo2 = {}
+        HowTo2["pos"] = {265, 55}
+        HowTo2["color"] = FontColour
+        HowTo2["text"] = "the table then left click to inspect the reagents inside the ingredient. Right-Click to open"
+        HowTo2["font"] = BodyFont
+        HowTo2["xalign"] = TEXT_ALIGN_CENTER
+        HowTo2["yalign"] = TEXT_ALIGN_TOP
+
+        HowTo3 = {}
+        HowTo3["pos"] = {265, 70}
+        HowTo3["color"] = FontColour
+        HowTo3["text"] = "the context menu. From there, transfer it into the table to begin brewing a potion."
+        HowTo3["font"] = BodyFont
+        HowTo3["xalign"] = TEXT_ALIGN_CENTER
+        HowTo3["yalign"] = TEXT_ALIGN_TOP
+
+
+        TipsTitle = {}
+        TipsTitle["pos"] = {265, 110}
+        TipsTitle["color"] = FontColour
+        TipsTitle["text"] = "Tips:"
+        TipsTitle["font"] = TitleFont
+        TipsTitle["xalign"] = TEXT_ALIGN_CENTER
+        TipsTitle["yalign"] = TEXT_ALIGN_TOP
+
+        Tips1 = {}
+        Tips1["pos"] = {265, 145}
+        Tips1["color"] = FontColour
+        Tips1["text"] = "Potions require powers of 3 for the next tier!"
+        Tips1["font"] = BodyFont
+        Tips1["xalign"] = TEXT_ALIGN_CENTER
+        Tips1["yalign"] = TEXT_ALIGN_TOP
+
+        Tips2 = {}
+        Tips2["pos"] = {265, 160}
+        Tips2["color"] = FontColour
+        Tips2["text"] = "Extra reagents in a potion are destroyed after brewing. Maximize tiers before brewing!"
+        Tips2["font"] = BodyFont
+        Tips2["xalign"] = TEXT_ALIGN_CENTER
+        Tips2["yalign"] = TEXT_ALIGN_TOP
+
+        Tips3 = {}
+        Tips3["pos"] = {265, 175}
+        Tips3["color"] = FontColour
+        Tips3["text"] = "Potions can be re-brewed for higher tiers. After-brewing, right click it for extra options"
+        Tips3["font"] = BodyFont
+        Tips3["xalign"] = TEXT_ALIGN_CENTER
+        Tips3["yalign"] = TEXT_ALIGN_TOP
         
 
-        draw.Text(struc, 1, 200)
-    end
-    local htuLine2 = vgui.Create("DLabel", helpFrame)
-    htuLine2:SetFont(BodyFont)
-    htuLine2:SetText("")
-    htuLine2:SetSize( ScrW() * 600/1920, ScrH() * 125/1080 )
-    htuLine2:SetPos(ScrW() * 0/1920, ScrH() * 0/1080)
-    htuLine2:SetTextColor(FontColour)
-    htuLine2.Paint = function(s, w, h)
         
-        struc = {}
-        struc["pos"] = {265, 55}
-        struc["color"] = FontColour
-        struc["text"] = "the table then left click to inspect the reagents inside the ingredient. Right-Click to open"
-        struc["font"] = BodyFont
-        struc["xalign"] = TEXT_ALIGN_CENTER
-        struc["yalign"] = TEXT_ALIGN_TOP
-        
-
-        draw.Text(struc, 1, 200)
-    end
-    local htuLine3 = vgui.Create("DLabel", helpFrame)
-    htuLine3:SetFont(BodyFont)
-    htuLine3:SetText("")
-    htuLine3:SetSize( ScrW() * 600/1920, ScrH() * 125/1080 )
-    htuLine3:SetPos(ScrW() * 0/1920, ScrH() * 0/1080)
-    htuLine3:SetTextColor(FontColour)
-    htuLine3.Paint = function(s, w, h)
-        
-        struc = {}
-        struc["pos"] = {265, 70}
-        struc["color"] = FontColour
-        struc["text"] = "the context menu. From there, transfer it into the table to begin brewing a potion."
-        struc["font"] = BodyFont
-        struc["xalign"] = TEXT_ALIGN_CENTER
-        struc["yalign"] = TEXT_ALIGN_TOP
-        
-
-        draw.Text(struc, 1, 200)
-    end
-
-    openAnimation = Derma_Anim("ExpandOpen", helpFrame, function(pnl, anim, delta, data)
-    
+        draw.Text(HowToTitle)
+        draw.Text(HowTo1)
+        draw.Text(HowTo2)
+        draw.Text(HowTo3)
+        draw.Text(TipsTitle)
+        draw.Text(Tips1)
+        draw.Text(Tips2)
+        draw.Text(Tips3)
+    end 
+   
+    local openAnimation = Derma_Anim("ExpandOpen", helpFrame, function(pnl, anim, delta, data)
         local maxHeight = 560
-        local maxWidth = 110
+        local maxWidth = 210
 
         local curHeight = math.floor(delta * maxHeight)
         local curWidth = math.floor(delta * maxWidth)
 
         pnl:SetSize(curHeight, curWidth)
-    
     end)
     
 
@@ -118,13 +132,7 @@ function DrawHelpMenu()
         if openAnimation:Active() then
             openAnimation:Run()
         end
-
-
     end
 
-
-    
     openAnimation:Start(0.15)
-
-
 end
