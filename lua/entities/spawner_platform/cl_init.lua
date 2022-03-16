4db4bb263a5d7ad880870063d444c5c210cd543b
Myth1c <Alemsm1@gmail.com>
2022-03-16 19:10:46 -0400

    Updated drawing conditions

--- lua/entities/spawner_platform/cl_init.lua
+++ lua/entities/spawner_platform/cl_init.lua
@@ -6,7 +6,7 @@ function ENT:Draw()
 
     if LocalPlayer():GetTool() == nil or !(LocalPlayer():Alive()) then return end
 
-    if LocalPlayer():GetTool().Command == "gmod_toolmode ingredient_spawner" and LocalPlayer():GetActiveWeapon():GetClass() == "gmod_tool" then
+    if LocalPlayer():GetTool().Name == "#ingredient_spawner" and LocalPlayer():GetActiveWeapon():GetClass() == "gmod_tool" then
 
 	    self:DrawModel()
 
