58738443a70c9a3f150d29cef5517f82dfbc43c1
Myth1c <Alemsm1@gmail.com>
2022-03-26 03:13:26 -0400

    Fixed spawners not drawing custom models on init

--- lua/entities/spawner_platform/cl_init.lua
+++ lua/entities/spawner_platform/cl_init.lua
@@ -3,10 +3,15 @@
 include("sh_init.lua")
 
 function ENT:Draw()
+    if self:GetCustomModel() then
+        self:DrawModel()
+        return
+    end
 
     if LocalPlayer():GetTool() == nil or !(LocalPlayer():Alive()) then return end
+    
 
-    if (LocalPlayer():GetTool().Name == "#ingredient_spawner" and LocalPlayer():GetActiveWeapon():GetClass() == "gmod_tool")  or self:GetCustomModel() then
+    if (LocalPlayer():GetTool().Name == "#ingredient_spawner" and LocalPlayer():GetActiveWeapon():GetClass() == "gmod_tool")then
 
 	    self:DrawModel()
 
